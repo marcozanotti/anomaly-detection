96409dd1250a186cd63631b2c45876424231a433
Marco Zanotti <zanottimarco17@gmail.com>
2025-08-20 11:07:49 +0200

    add utils R functions

--- src/R/test.R
+++ src/R/test.R
@@ -1,10 +1,21 @@
+reticulate::use_condaenv('global_retrain')
+
 library(tidyverse)
-remotes::install_github("robjhyndman/weird-package")
+library(weird)
+
+reticulate::source_python('src/Python/utils/utilities.py')
+source('src/R/utils.R')
+
+data = get_data(path_list = c('data', 'nab'), name_list = c('nab', 'prep'))
+data = data |> 
+    filter(unique_id == "speed_7578")
+plot(x = data$ds, y = data$y, type = "l")
 
 
-otsad::nyc_taxi |> str()
-otsad::nyc_taxi$is.real.anomaly |> table()
+weird::
 
 
-plot(x = 1:nrow(otsad::nyc_taxi), y = otsad::nyc_taxi$value, type = 'l')
+
+
+
 
