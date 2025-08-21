a71513007f1d8e7f77991e6e6fb972f2b1df64a0
Marco Zanotti <zanottimarco17@gmail.com>
2025-08-21 13:30:11 +0200

    add weird methods

--- src/R/test.R
+++ src/R/test.R
@@ -1,21 +1,110 @@
 reticulate::use_condaenv('global_retrain')
 
+reticulate::source_python('src/Python/utils/utilities.py')
+source('src/R/utils.R')
+source('src/R/anomaly_detection.R')
+
 library(tidyverse)
 library(weird)
 
-reticulate::source_python('src/Python/utils/utilities.py')
-source('src/R/utils.R')
 
+# Load data
 data = get_data(path_list = c('data', 'nab'), name_list = c('nab', 'prep'))
-data = data |> 
-    filter(unique_id == "speed_7578")
-plot(x = data$ds, y = data$y, type = "l")
+data = data |> filter(unique_id == "speed_7578")
 
-
-weird::
+plot_anomalies(data, anomaly_column = 'is_real_anomaly')
 
 
 
+# Statistical Tests
+zscore_anomalies(data$y, q = 3)
+weird::chauvenet_anomalies(data$y)
+weird::peirce_anomalies(data$y)
+weird::dixon_anomalies(data$y, alpha = 0.05)
+weird::grubbs_anomalies(data$y, alpha = 0.05)
 
 
 
+# Boxplots
+tukey_anomaly(data$y, extreme = TRUE)
+barbato_anomaly(data$y, extreme = TRUE)
+
+
+
+# Density-based Methods
+
+# specific distribution
+weird::surprisals(
+    data$y, 
+    distribution = distributional::dist_normal(), 
+    approximation = 'empirical',
+    loo = TRUE
+)
+data |>
+    dplyr::mutate(
+        prob = weird::surprisals(
+            data$y, distribution = distributional::dist_normal(), 
+            approximation = 'empirical', loo = TRUE
+        )
+    ) |> 
+    ggplot(aes(x = ds, y = y, color = prob < 0.05)) +
+    geom_jitter(height = 0, width = 0.2) +
+    scale_y_log10()
+
+# linear regression
+fit_lm <- lm(y ~ as.numeric(ds), data = data)
+weird::surprisals(fit_lm, approximation = 'empirical', loo = TRUE)
+data |>
+    dplyr::mutate(prob = weird::surprisals(fit_lm, approximation = 'empirical', loo = TRUE)) |> 
+    ggplot(aes(x = ds, y = y, color = prob < 0.05)) +
+    geom_jitter(height = 0, width = 0.2) +
+    scale_y_log10()
+
+# gam regression
+fit_gam <- mgcv::gam(y ~ as.numeric(ds), data = data)
+weird::surprisals(fit_gam, approximation = 'empirical')
+data |>
+    dplyr::mutate(prob = weird::surprisals(fit_gam, approximation = 'empirical')) |> 
+    ggplot(aes(x = ds, y = y, color = prob < 0.05)) +
+    geom_jitter(height = 0, width = 0.2) +
+    scale_y_log10()
+
+# KDE
+weird::surprisals(data$y, approximation = 'empirical', loo =  TRUE)
+data |>
+    dplyr::mutate(prob = weird::surprisals(data$y, approximation = 'empirical', loo =  TRUE)) |> 
+    ggplot(aes(x = ds, y = y, color = prob < 0.005)) +
+    geom_jitter(height = 0, width = 0.2) +
+    scale_y_log10()
+# weird::gg_hdrboxplot(
+#     data, var1 = 'y', show_points = TRUE,
+#     show_anomalies = TRUE
+# )
+
+weird::lookout_prob(data$y)
+data |>
+    dplyr::mutate(prob = weird::lookout_prob(data$y)) |> 
+    ggplot(aes(x = ds, y = y, color = prob < 0.13)) +
+    geom_jitter(height = 0, width = 0.2) +
+    scale_y_log10()
+
+
+# Distance-based Methods
+weird::lof_scores(data$y)
+weird::lof_scores(data$y, k = 10) > 1 # outliers if greater than 1
+data |>
+    dplyr::mutate(prob = weird::lof_scores(data$y, k = 5)) |> 
+    ggplot(aes(x = ds, y = y, color = prob > 1)) +
+    geom_jitter(height = 0, width = 0.2) +
+    scale_y_log10()
+
+weird::stray_scores(data$y)
+weird::stray_anomalies(data$y)
+data |>
+    dplyr::mutate(prob = weird::stray_scores(data$y)) |> 
+    ggplot(aes(x = ds, y = y, color = prob < 0.01)) +
+    geom_jitter(height = 0, width = 0.2) +
+    scale_y_log10()
+data |>
+    dplyr::mutate(is_anomaly = weird::stray_anomalies(data$y)) |> 
+    plot_anomalies(anomaly_column = 'is_anomaly')
--- src/R/utils.R
+++ src/R/utils.R
@@ -1,81 +1,81 @@
 
 remove_series <- function(data, min_series_length) {
   
-  # Compute series length
-  series_length <- data |> 
-    dplyr::group_by(unique_id) |> 
-    dplyr::count()
-  
-  message(sprintf("Removing series shorter than %d observations...", min_series_length))
-  
-  # IDs to remove
-  remove_ids <- series_length |> 
-    dplyr::filter(n < min_series_length) |> 
-    dplyr::pull(unique_id)
-  
-  # Filter dataset
-  res_df <- data |> filter(!unique_id %in% remove_ids)
-  
-  # Summary stats
-  n_series <- length(unique(data$unique_id))
-  n_series_final <- length(unique(res_df$unique_id))
-  n_series_to_remove <- n_series - n_series_final
-  p_series_to_remove <- n_series_to_remove / n_series * 100
-  
-  message(sprintf(
-    "Removed %d series out of %d (%.1f%%).", 
-    n_series_to_remove, n_series, p_series_to_remove
-  ))
-  message(sprintf("The final dataset contains %d series.", n_series_final))
-  
-  return(res_df)
+    # Compute series length
+    series_length <- data |> 
+      dplyr::group_by(unique_id) |> 
+      dplyr::count()
+    
+    message(sprintf("Removing series shorter than %d observations...", min_series_length))
+    
+    # IDs to remove
+    remove_ids <- series_length |> 
+      dplyr::filter(n < min_series_length) |> 
+      dplyr::pull(unique_id)
+    
+    # Filter dataset
+    res_df <- data |> filter(!unique_id %in% remove_ids)
+    
+    # Summary stats
+    n_series <- length(unique(data$unique_id))
+    n_series_final <- length(unique(res_df$unique_id))
+    n_series_to_remove <- n_series - n_series_final
+    p_series_to_remove <- n_series_to_remove / n_series * 100
+    
+    message(sprintf(
+      "Removed %d series out of %d (%.1f%%).", 
+      n_series_to_remove, n_series, p_series_to_remove
+    ))
+    message(sprintf("The final dataset contains %d series.", n_series_final))
+    
+    return(res_df)
 }
 
 filter_series <- function(data, max_series_length) {
   
-  message(sprintf("Filtering series longer than %d observations...", max_series_length))
-  
-  res_df <- data |> 
-    dplyr::arrange(unique_id, ds) |> 
-    dplyr::group_by(unique_id) |> 
-    dplyr::slice_tail(n = max_series_length) |> 
-    dplyr::ungroup()
-  
-  # Summary stats
-  n_obs <- nrow(data)
-  n_obs_final <- nrow(res_df)
-  n_obs_to_remove <- n_obs - n_obs_final
-  p_obs_to_remove <- n_obs_to_remove / n_obs * 100
-  
-  message(sprintf(
-    "Removed %d observations out of %d (%.1f%%).", 
-    n_obs_to_remove, n_obs, p_obs_to_remove
-  ))
-  message(sprintf("The final dataset contains %d observations.", n_obs_final))
-  
-  return(res_df)
+    message(sprintf("Filtering series longer than %d observations...", max_series_length))
+    
+    res_df <- data |> 
+      dplyr::arrange(unique_id, ds) |> 
+      dplyr::group_by(unique_id) |> 
+      dplyr::slice_tail(n = max_series_length) |> 
+      dplyr::ungroup()
+    
+    # Summary stats
+    n_obs <- nrow(data)
+    n_obs_final <- nrow(res_df)
+    n_obs_to_remove <- n_obs - n_obs_final
+    p_obs_to_remove <- n_obs_to_remove / n_obs * 100
+    
+    message(sprintf(
+      "Removed %d observations out of %d (%.1f%%).", 
+      n_obs_to_remove, n_obs, p_obs_to_remove
+    ))
+    message(sprintf("The final dataset contains %d observations.", n_obs_final))
+    
+    return(res_df)
 }
 
 sampling_data <- function(data, samples = 1000) {
   
-  message(sprintf("Sampling %d series from data...", samples))
-  
-  ids <- unique(data$unique_id)
-  sample_ids <- sample(ids, size = samples, replace = FALSE)
-  
-  res_df <- data |>
-    dplyr::filter(unique_id %in% sample_ids)
-  
-  return(res_df)
+    message(sprintf("Sampling %d series from data...", samples))
+    
+    ids <- unique(data$unique_id)
+    sample_ids <- sample(ids, size = samples, replace = FALSE)
+    
+    res_df <- data |>
+      dplyr::filter(unique_id %in% sample_ids)
+    
+    return(res_df)
 }
 
 get_data <- function(
-  path_list, 
-  name_list, 
-  min_series_length = NULL, 
-  max_series_length = NULL, 
-  samples = NULL, 
-  ext = '.parquet'
+    path_list, 
+    name_list, 
+    min_series_length = NULL, 
+    max_series_length = NULL, 
+    samples = NULL, 
+    ext = '.parquet'
 ) {
 
     res_df = load_data(path_list, name_list, ext)
@@ -100,70 +100,70 @@ get_data <- function(
 
 get_aggregate_function <- function(function_name) {
 
-  if (function_name == 'mean') {
-    return(mean)
-  } else if (function_name == 'median') {
-    return(median)
-  } else if(function_name == 'std') {
-    return(sd)
-  } else if (function_name == 'max') {
-    return(max)
-  } else if(function_name =='min') {
-    return(min)
-  } else if(function_name == 'sum') {
-    return(sum)
-  } else {
-    stop('Invalid aggregate function')
-  }
-  
+    if (function_name == 'mean') {
+      return(mean)
+    } else if (function_name == 'median') {
+      return(median)
+    } else if(function_name == 'std') {
+      return(sd)
+    } else if (function_name == 'max') {
+      return(max)
+    } else if(function_name =='min') {
+      return(min)
+    } else if(function_name == 'sum') {
+      return(sum)
+    } else {
+      stop('Invalid aggregate function')
+    }
+    
 }
 
 aggregate_data <- function(
-  data, 
-  group_columns, 
-  drop_columns = NULL, 
-  function_name = 'median'
+    data, 
+    group_columns, 
+    drop_columns = NULL, 
+    function_name = 'median'
 ) {
 
-  data_agg = data
+    data_agg = data
 
-  if (!is.null(drop_columns)) {
+    if (!is.null(drop_columns)) {
+      data_agg <- data_agg |> 
+        dplyr::select(-dplyr::any_of(drop_columns))
+    }
+
+    agg_fun <- get_aggregate_function(function_name)
+
     data_agg <- data_agg |> 
-      dplyr::select(-dplyr::any_of(drop_columns))
-  }
+      dplyr::group_by(!!!rlang::syms(group_columns)) |> 
+      dplyr::summarise(dplyr::across(where(is.numeric), agg_fun), .groups = 'drop')
 
-  agg_fun <- get_aggregate_function(function_name)
-
-  data_agg <- data_agg |> 
-    dplyr::group_by(!!!rlang::syms(group_columns)) |> 
-    dplyr::summarise(dplyr::across(where(is.numeric), agg_fun), .groups = 'drop')
-
-  return(data_agg)
+    return(data_agg)
 
 }
 
 get_model_type <- function(model_name) {
 
-  sf <- c('ETS', 'ARIMA')
-  ml <- c(
-    'LinearRegression', 'Lasso', 'Ridge', 
-    'RandomForestRegressor', 
-    'XGBRegressor', 'LGBMRegressor', 'CatBoostRegressor' 
-  )
-  dl <- c('MLP', 'LSTM', 'TCN', 'NBEATSx', 'NHITS')
-  ens_acc <- c('EnsembleMean2A', 'EnsembleMean3A', 'EnsembleMean4A', 'EnsembleMean5A')
-  ens_time <- c('EnsembleMean2T', 'EnsembleMean3T', 'EnsembleMean4T', 'EnsembleMean5T')
+    sf <- c('ETS', 'ARIMA')
+    ml <- c(
+      'LinearRegression', 'Lasso', 'Ridge', 
+      'RandomForestRegressor', 
+      'XGBRegressor', 'LGBMRegressor', 'CatBoostRegressor' 
+    )
+    dl <- c('MLP', 'LSTM', 'TCN', 'NBEATSx', 'NHITS')
+    ens_acc <- c('EnsembleMean2A', 'EnsembleMean3A', 'EnsembleMean4A', 'EnsembleMean5A')
+    ens_time <- c('EnsembleMean2T', 'EnsembleMean3T', 'EnsembleMean4T', 'EnsembleMean5T')
 
-  model_type <- dplyr::case_when(
-    model_name %in% sf ~ 'SF',
-    model_name %in% ml ~ 'ML',
-    model_name %in% dl ~ 'DL',
-    model_name %in% ens_acc ~ 'ENSACC',
-    model_name %in% ens_time ~ 'ENSTIME',
-    TRUE ~ NA_character_
-  )
+    model_type <- dplyr::case_when(
+      model_name %in% sf ~ 'SF',
+      model_name %in% ml ~ 'ML',
+      model_name %in% dl ~ 'DL',
+      model_name %in% ens_acc ~ 'ENSACC',
+      model_name %in% ens_time ~ 'ENSTIME',
+      TRUE ~ NA_character_
+    )
 
-  return(model_type)
+    return(model_type)
 
 }
 
@@ -196,64 +196,64 @@ get_model_name_abbr <- function(model_name) {
 
 recode_data <- function(data, model_type_levels, model_names_abbr) {
 
-  data_recoded <- data |>  
-    dplyr::mutate(
-      type = get_model_type(method),
-      type = factor(type, levels = model_type_levels, ordered = TRUE),
-      .before = 'method',
-    ) |> 
-    dplyr::mutate(
-      method = factor(get_model_name_abbr(method), levels = model_names_abbr, ordered = TRUE)
-    ) |> 
-    dplyr::arrange(type, method)
-  return(data_recoded)
+    data_recoded <- data |>  
+      dplyr::mutate(
+        type = get_model_type(method),
+        type = factor(type, levels = model_type_levels, ordered = TRUE),
+        .before = 'method',
+      ) |> 
+      dplyr::mutate(
+        method = factor(get_model_name_abbr(method), levels = model_names_abbr, ordered = TRUE)
+      ) |> 
+      dplyr::arrange(type, method)
+    return(data_recoded)
 
 }
 
 dt_table <- function(data, title = "", caption = "", rownames = FALSE, digits = 2, format = 'numeric') {
 	
-	p_len <- nrow(data)
-	
-	if (format == 'dollar') {
-		res_data <- data |>
-			dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = digits))) |>  
-			dplyr::mutate(dplyr::across(where(is.numeric), ~ scales::dollar(.x, big.mark = ",", decimal.mark = '.')))
-	} else if (format == 'percent') {
-		res_data <- data |>
-			dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = digits))) |>  
-			dplyr::mutate(dplyr::across(where(is.numeric), ~ scales::percent(.x, scale = 1)))
-	} else {
-		res_data <- data |>
-			dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = digits)))
-	}
-	
-	res <- res_data |> 
-		DT::datatable(
-			extensions = "Buttons",
-			# filter = "top", # for filtering enable searching and lenghtChange
-			options = list(
-				pageLength = p_len,
-				paging = FALSE,
-				searching = FALSE,
-				ordering = FALSE,
-				lenghtChange = FALSE,
-				autoWidth = FALSE,
-				dom = "Bfrtip",
-				buttons = c("copy", "print", "csv", "excel", "pdf"),
-				drawCallback = DT::JS(
-					c(
-						"function(settings){",
-						"  var datatable = settings.oInstance.api();",
-						"  var table = datatable.table().node();",
-						paste0("  var caption = '", caption, "'"),
-						"  $(table).append('<caption style=\"caption-side: bottom\">' + caption + '</caption>');",
-						"}"
-					)
-				)
-			),
-			rownames = rownames,
-			caption = title
-		)
-	return(res)
+    p_len <- nrow(data)
+    
+    if (format == 'dollar') {
+      res_data <- data |>
+        dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = digits))) |>  
+        dplyr::mutate(dplyr::across(where(is.numeric), ~ scales::dollar(.x, big.mark = ",", decimal.mark = '.')))
+    } else if (format == 'percent') {
+      res_data <- data |>
+        dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = digits))) |>  
+        dplyr::mutate(dplyr::across(where(is.numeric), ~ scales::percent(.x, scale = 1)))
+    } else {
+      res_data <- data |>
+        dplyr::mutate(dplyr::across(where(is.numeric), ~ round(.x, digits = digits)))
+    }
+    
+    res <- res_data |> 
+      DT::datatable(
+        extensions = "Buttons",
+        # filter = "top", # for filtering enable searching and lenghtChange
+        options = list(
+          pageLength = p_len,
+          paging = FALSE,
+          searching = FALSE,
+          ordering = FALSE,
+          lenghtChange = FALSE,
+          autoWidth = FALSE,
+          dom = "Bfrtip",
+          buttons = c("copy", "print", "csv", "excel", "pdf"),
+          drawCallback = DT::JS(
+            c(
+              "function(settings){",
+              "  var datatable = settings.oInstance.api();",
+              "  var table = datatable.table().node();",
+              paste0("  var caption = '", caption, "'"),
+              "  $(table).append('<caption style=\"caption-side: bottom\">' + caption + '</caption>');",
+              "}"
+            )
+          )
+        ),
+        rownames = rownames,
+        caption = title
+      )
+    return(res)
 	
 }
